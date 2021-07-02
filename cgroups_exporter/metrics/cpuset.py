c00ea3f98299b958b59930a20ace5f425f549780
Dmitry Orlov <me@mosquito.su>
2021-07-02 14:59:32 +0300

    add cpuset and bump to 0.3.2

---
+++ cgroups_exporter/metrics/cpuset.py
@@ -0,0 +1,53 @@
+import logging
+
+from .base import CGroupTask, StatBase, IntProviderBase
+
+log = logging.getLogger()
+
+
+def cpu_collector(task: CGroupTask):
+    for collector in COLLECTORS:
+        try:
+            collector(task)()
+        except Exception:
+            log.exception("Failed to collect %r", collector)
+
+
+class CPUAcctStat(StatBase):
+    STAT_FILE = "cpuacct.stat"
+    DOCUMENTATION = "CPU accounting statistic"
+
+
+class CPUStat(StatBase):
+    STAT_FILE = "cpu.stat"
+    DOCUMENTATION = "CPU statistic"
+
+
+class CPUCFSPeriods(IntProviderBase):
+    FILENAME = "cpu.cfs_period_us"
+    NAME = "cfs"
+    METRIC = "period_us"
+    DOCUMENTATION = "Allowed CPU periods in microseconds"
+
+
+class CPUCFSQuota(IntProviderBase):
+    FILENAME = "cpu.cfs_quota_us"
+    NAME = "cfs"
+    METRIC = "quota_us"
+    DOCUMENTATION = "Allowed CPU quota in microseconds"
+
+
+class CPUShares(IntProviderBase):
+    FILENAME = "cpu.shares"
+    NAME = "shares"
+    METRIC = None
+    DOCUMENTATION = "Allowed CPU shares"
+
+
+COLLECTORS = (
+    CPUStat,
+    CPUAcctStat,
+    CPUCFSPeriods,
+    CPUCFSQuota,
+    CPUShares,
+)
